63029f422bad0ecf326419b4f3baf407a30c1e76
Pa4ok <n_leontiev@mail.ru>
2022-10-12 01:57:11 +0300

    add lab2

---
+++ Lab2/src/main/java/ru/pa4ok/lab2/Application.java
@@ -0,0 +1,53 @@
+package ru.pa4ok.lab2;
+
+import java.util.*;
+
+/**
+ * Для удобства расчетов растояние считается в см
+ * Тогда, если скорость 1см/сек, дистация будет равна времени прохождения
+ * n - количество улиток
+ *
+ * количество вычислений расстояния между улитками достигается меньше чем n^2
+ * улитки, которые запутались или идут навстречу, пропускаются при дальнейших поисках ближайших
+ *
+ * когда улитка e1 нашли ближайшую улитку e2
+ * если у e2 еще не найдена ближайшая улитка, и она сама не запуталась, вызывается ее поиск
+ * после этого, если e1 и e2 нашли друг друга, они исключаются из дальнейших сравнений
+ */
+public class Application
+{
+    public static final Random RAND = new Random();
+    public static final int BORDER = 100_000;
+    public static final int ENTITY_COUNT = 50 + RAND.nextInt(25);
+
+    public static void main(String[] args)
+    {
+        Entity[] entities = new Entity[ENTITY_COUNT];
+        for(int i=0; i<entities.length; i++) {
+            entities[i] = new Entity(RAND.nextInt(BORDER), RAND.nextInt(BORDER));
+        }
+
+        double minDistance = Double.MAX_VALUE;
+        for(Entity e : entities) {
+            if(e.findClosest(entities)) {
+                Entity closest = e.getClosest();
+                if(closest.getClosest() == null && !closest.isBlocked()) {
+                    closest.findClosest(entities);
+                }
+                if(e == closest.getClosest()) {
+                    e.setDoubleFound(true);
+                    closest.getClosest().setDoubleFound(true);
+                    minDistance = Math.min(minDistance, e.getClosestDistance() / 2);
+                } else {
+                    minDistance = Math.min(minDistance, e.getClosestDistance());
+                }
+            }
+        }
+
+        if(minDistance != Double.MAX_VALUE) {
+            System.out.println("Минимальное время " + (int)Math.ceil(minDistance) + " сек.");
+        } else {
+            System.out.println("Все улитки запутались в своей бинарности <clown>");
+        }
+    }
+}
---
+++ Lab2/src/main/java/ru/pa4ok/lab2/Entity.java
@@ -0,0 +1,63 @@
+package ru.pa4ok.lab2;
+
+import lombok.Data;
+
+import java.util.Objects;
+
+@Data
+public class Entity
+{
+    protected double x;
+    protected double z;
+
+    protected transient Entity closest = null;
+    protected transient double closestDistance = -1;
+    protected transient boolean blocked = false;
+    protected transient boolean doubleFound = false;
+
+    public Entity(double x, double z) {
+        this.x = x;
+        this.z = z;
+    }
+
+    public boolean findClosest(Entity[] entities)
+    {
+        if(this.blocked || this.doubleFound) {
+            return false;
+        }
+
+        Entity closest = null;
+        double closestDistance = Double.MAX_VALUE;
+        double closestCount = 0;
+
+        for(Entity e : entities) {
+            if(e != this && !e.blocked && !e.doubleFound) {
+                double distance = this.getDistance(e);
+                if(distance < closestDistance) {
+                    closest = e;
+                    closestDistance = distance;
+                    closestCount = 1;
+                } else if(distance == closestDistance) {
+                    closestCount++;
+                }
+            }
+        }
+
+        if(closest != null) {
+            if(closestCount == 1) {
+                this.closest = closest;
+                this.closestDistance = closestDistance;
+                return true;
+            }
+            this.blocked = true;
+        }
+
+        return false;
+    }
+
+    public double getDistance(Entity other) {
+        double d0 = other.x - x;
+        double d1 = other.z - z;
+        return Math.sqrt(d0 * d0 + d1 * d1);
+    }
+}
